601b8d4794176b0476e82f9311ac62e097fa570d
Mathias Schneider <mathy.sch.2007@gmail.com>
2026-04-19 20:42:49 -0300

    Implemented Infinite Mode

--- Scenes/MazeScene.cs
+++ Scenes/MazeScene.cs
@@ -14,19 +14,26 @@ namespace UnfathomableMaze.Scenes
         private Point finishFlag;
         private bool hardmode;
         private bool death;
+        private int score;
 
-        public MazeScene(bool hardmode)
+        public MazeScene(bool hardmode, int score = 0)
         {
             maze = new nMaze(mapGenerator);
             displayMap = maze.ConvertMap();
             player = new Point(1, 1);
             finishFlag = new Point(29, 29);
-            hardmode = this.hardmode;
+            this.hardmode = hardmode;
+            this.score = score;
+            
+            
         }
 
         public void Draw(Engine.Canvas canvas)
         {
             canvas.Clear();
+
+            // Score
+            canvas.Draw($"Score: {score}", 1, 1);
 
             // The visual map
             for (int i = 0; i < displayMap.GetLength(0); i++)
@@ -42,6 +49,18 @@ namespace UnfathomableMaze.Scenes
 
             // Player
             canvas.Draw("@", canvas.Width / 2 - displayMap.GetLength(1) / 2 + player.X, canvas.Height / 2 - displayMap.GetLength(0) / 2 + player.Y);
+
+            // Infinte Mode
+            if (player == finishFlag)
+            {
+                score = score + 1;
+                IScene? newScene = null;
+                if (hardmode)
+                {
+                    newScene = new MazeScene(true, score);
+                } else newScene = new MazeScene(false, score);
+                Engine.Instance?.UpdateScene(newScene);
+            }
         }
 
         public void OnKeyPressed(ConsoleKey key)
