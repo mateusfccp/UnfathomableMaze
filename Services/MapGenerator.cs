a7fb5db0886dd2c945ea1a61bdb3272de624a2c7
Mateus Pinto <mateusfccp@gmail.com>
2026-04-19 13:31:13 -0300

    Fix warnings and reformat code

--- Services/MapGenerator.cs
+++ Services/MapGenerator.cs
@@ -1,73 +1,99 @@
+using System.Diagnostics;
 using System.Drawing;
 using UnfathomableMaze.Enums;
 using UnfathomableMaze.Interfaces;
 
-namespace UnfathomableMaze.Services
+namespace UnfathomableMaze.Services;
+
+/// <summary>
+/// A generator for a map of tiles.
+///
+/// This class uses a depth-first search that uses a stack to keep track of the current path. The "ant" starts at a
+/// random position and carves a path through the maze until it reaches a dead end. When it reaches a dead end, it
+/// backtracks until it finds a new path to carve. This process continues until all paths have been carved and the maze
+/// is complete.
+///
+/// This class generates a maze with a fixed size of 51x51 tiles.
+/// </summary>
+public class MapGenerator : IMapTilesGenerator
 {
-    public class MapGenerator : IMapTilesGenerator
+    private const int Width = 51; // IMPORTANT: The size of the maze must ALWAYS be made by 2 odd numbers
+    private const int Height = 51;
+
+    /// <summary>
+    /// DETAIL: The number 51 means that the maze is composed by 49 real tiles.
+    /// The borders of the matrix will always be walls. This leaves us with 2 tiles less for each axis.
+    /// This means that if the player starts at position 1,1 (not 0,0 bc its wall) the finish point will be at index 49, 49.
+    ///</summary>
+    private readonly Random _random = new();
+
+    public Tile[,] Generate() // Function that initializes and generates the maze
     {
-        private readonly int _width = 51;    // IMPORTANT: The size of the maze must ALWAYS be made by 2 odd numbers
-        private readonly int _height = 51;
-        /// <summary>
-        /// DETAIL: The number 51 means that the maze is composed by 49 real tiles.
-        /// The borders of the matrix will always be walls. This leaves us with 2 tiles less for each axis.
-        /// This means that if the player starts at position 1,1 (not 0,0 bc its wall) the finish point will be at index 49, 49.
-        ///</summary>
-        private readonly Random _random = new();
+        Debug.Assert(Width % 2 == 1 && Height % 2 == 1, "The size of the maze must be made by 2 odd numbers.");
 
+        var map = new Tile[Width, Height]; // Creation of the matrix
 
-        public Tile[,] Generate()       // Function that initializates and generates the maze
+        for (var x = 0; x < Width; x++) // Fully initialized with walls
         {
-            Tile[,] map = new Tile[_width, _height];        // Creation of the matrix
-
-            for (int x = 0; x < _width; x++)             // Fully initializated with walls
+            for (var y = 0; y < Height; y++)
             {
-                for (int y = 0; y < _height; y++)
-                {
-                    map[x, y] = Tile.Wall;
-                }
+                map[x, y] = Tile.Wall;
             }
-
-            Stack<Point> stack = new Stack<Point>(); // Stack of positions the "ant" carved into the maze
-            int startX = 1;         // Start position of the player in the maze
-            int startY = 1;         // Can be changed but MUST ALWAYS be composed by two odd numbers.
-
-            map[startX, startY] = Tile.Path;       // Start position is turned into a path and pushed into the stack
-            stack.Push(new Point(startX, startY));          // This begins the "carving"
-
-            while (stack.Count > 0)
-            {
-                var current = stack.Peek();     // Buffer for the current tile
-                var neighbors = GetUnvisitedNeighbors(current.X, current.Y, map); // Buffer for the current list of neighbors. The var type is list.
-
-                if (neighbors.Count > 0)
-                {
-                    var next = neighbors[_random.Next(neighbors.Count)]; // If there are neighbors the ant chooses one randomly and goes there.
-
-                    map[next.X, next.Y] = Tile.Path;    // Turns the chosen neighbor and turns it into walkable path.
-                    map[current.X + (next.X - current.X) / 2, current.Y + (next.Y - current.Y) / 2] = Tile.Path; // Turns the space between the current tile and the chosen into path.
-
-                    stack.Push(next);   // The stack is updated and advances to the next tile.
-                }
-                else
-                {
-                    stack.Pop();    // If no neighbors are left the stack goes backwards and keeps checking for neighbors.
-                }
-            }
-
-            return map;
         }
 
-        private List<Point> GetUnvisitedNeighbors(int x, int y, Tile[,] map)
+        var stack = new Stack<Point>(); // Stack of positions the "ant" carved into the maze
+        const int startX = 1; // Start position of the player in the maze
+        const int startY = 1; // Can be changed but MUST ALWAYS be composed by two odd numbers.
+
+        map[startX, startY] = Tile.Path; // Start position is turned into a path and pushed into the stack
+        stack.Push(new Point(startX, startY)); // This begins the "carving"
+
+        while (stack.Count > 0)
         {
-            List<Point> neighbors = new List<Point>();    // Generates a list with all the posible options the ant can take in the next step.
+            var current = stack.Peek(); // Buffer for the current tile
 
-            if (x > 2 && map[x - 2, y] == Tile.Wall) neighbors.Add(new Point(x - 2, y)); // Checks for the limit of the matrix at the left and neighbors at the left of X 
-            if (x < _width - 2 && map[x + 2, y] == Tile.Wall) neighbors.Add(new Point(x + 2, y)); // Checks for the limit of the matrix at the right and neighbors at the right of X 
-            if (y > 2 && map[x, y - 2] == Tile.Wall) neighbors.Add(new Point(x, y - 2)); // Checks for the limit of the matrix at the top and neighbors at the top of Y
-            if (y < _height - 2 && map[x, y + 2] == Tile.Wall) neighbors.Add(new Point(x, y + 2)); // Checks for the limit of the matrix at the bot and neighbors at the bot of Y
+            // Buffer for the current list of neighbors. The var type is list.
+            var neighbors = GetUnvisitedNeighbors(current.X, current.Y, map);
 
-            return neighbors;
+            if (neighbors.Count > 0)
+            {
+                // If there are neighbors, the ant chooses one randomly and goes there.
+                var next = neighbors[_random.Next(neighbors.Count)];
+
+                map[next.X, next.Y] = Tile.Path; // Turns the chosen neighbor and turns it into walkable path.
+
+                // Turns the space between the current tile and the chosen into path.
+                map[current.X + (next.X - current.X) / 2, current.Y + (next.Y - current.Y) / 2] = Tile.Path;
+
+                stack.Push(next); // The stack is updated and advances to the next tile.
+            }
+            else
+            {
+                stack.Pop(); // If no neighbors are left, the stack goes backwards and keeps checking for neighbors.
+            }
         }
+
+        return map;
+    }
+
+    private List<Point> GetUnvisitedNeighbors(int x, int y, Tile[,] map)
+    {
+        // Generates a list with all the possible options the ant can take in the next step.
+        List<Point> neighbors = [];
+
+        if (x > 2 && map[x - 2, y] == Tile.Wall)
+            // Checks for the limit of the matrix at the left and neighbors at the left of X
+            neighbors.Add(new Point(x - 2, y));
+        if (x < Width - 2 && map[x + 2, y] == Tile.Wall)
+            // Checks for the limit of the matrix at the right and neighbors at the right of X
+            neighbors.Add(new Point(x + 2, y));
+        if (y > 2 && map[x, y - 2] == Tile.Wall)
+            // Checks for the limit of the matrix at the top and neighbors at the top of Y
+            neighbors.Add(new Point(x, y - 2));
+        if (y < Height - 2 && map[x, y + 2] == Tile.Wall)
+            // Checks for the limit of the matrix at the bot and neighbors at the bot of Y
+            neighbors.Add(new Point(x, y + 2));
+
+        return neighbors;
     }
 }
